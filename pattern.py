b7e9ab782f602cf273efd0e308f89de45e0abadd
AnanyaDevInProgress <ananya.dr1998@gmail.com>
2026-05-26 10:43:55 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,6 +1,6 @@
 # Just a pattern code
 n = 9
-print("My first E2E Jenkins run")
+print("My first E2E Jenkins run!!!!")
 for i in range(n):
   print(" " * (n - i - 1), end="")
   print("*" * (2 * i + 1))
